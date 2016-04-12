119510295f522a339ec69f076fab5f782e33eb8b
Lhacker <starclown1@gmail.com>
2016-04-12 21:05:47 +0900

    implement login post method

--- webtodo/todo/urls.py
+++ webtodo/todo/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name='todo'
 urlpatterns = [
     url(r'^login/$', views.login, name='login'),
-    url(r'^do_login/$', views.do_login, name='do_login'),
     url(r'^$', views.index, name='index'),
 ]
--- webtodo/todo/views.py
+++ webtodo/todo/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 def login(request):
-    return render(request, 'todo/login.html', {})
+    from django.contrib.auth import login
 
-def do_login(request):
-    return HttpResponse("do_login")
+    user = None
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+
+        if user is not None and user.is_active:
+            login(request, user)
+            return HttpResponseRedirect(reverse('todo:index'))
+
+    return render(request, 'todo/login.html', {
+        'user': user
+    })
 
 def index(request):
     return HttpResponse("index")
