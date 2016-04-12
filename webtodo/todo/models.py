7c5fe23a7b232315350de93cace3c8f1025e0a7d
Lhacker <starclown1@gmail.com>
2016-04-12 20:08:41 +0900

    delete "User" table

--- webtodo/todo/models.py
+++ webtodo/todo/models.py
@@ -1,15 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.username
 
 class Todo(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
