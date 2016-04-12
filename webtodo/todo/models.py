389830fbb7a98c713ad7a5c7c05897249a5b715a
Lhacker <starclown1@gmail.com>
2016-04-12 16:47:42 +0900

    update model, and migrate to db

--- webtodo/todo/models.py
+++ webtodo/todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
+
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    todo_text = models.CharField(max_length=255)
+    order = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.todo_text
